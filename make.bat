b8846bc2784c2847b7d1dbc7cca55ca86304aefb
128na <simutrans.128na@gmail.com>
2017-04-16 17:44:56 +0900

    add files.

---
+++ make.bat
@@ -0,0 +1,29 @@
+@echo off
+chcp 65001
+
+rem この行はコメントです
+
+rem 設定項目
+rem pak化するサイズを指定。64の場合は空にします。
+rem set PAK_SIZE=
+set PAK_SIZE=128
+rem pakフォルダ名を指定。
+set PAK_DIR=pak128
+rem simutransの実行ファイル名を指定（拡張子も含む）
+set SIMU_EXE=simutrans.exe
+
+cd source
+
+rem LOOPからGOTO LOOPまでの間が繰り返し実行されます。
+:LOOP
+
+  echo PAk化を実行します
+  echo -------------------
+  ..\makeobj55.exe pak%PAK_SIZE%
+  echo -------------------
+
+  copy *.pak ..\..\%PAK_DIR%\
+
+  ..\..\%SIMU_EXE%
+
+GOTO LOOP
---
+++ readme.md
@@ -0,0 +1,13 @@
+# Auto Pak Builder / アドオン作成自動化ツール
+
+## 使い方
+
+addon_pak_builderフォルダを下記のように配置してください。
+
+```
+simutrans
+┣simutrans.exe
+┣pakまたはpak128
+┗addon_pak_builder/
+```
+
